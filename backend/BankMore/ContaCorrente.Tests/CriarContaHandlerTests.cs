deb74cb0688d41a13bcf2f9550182c60777b8dcc
kmendess <karolmendes.rj@gmail.com>
2026-03-25 23:59:41 -0300

    test: adiciona testes para CriarContaHandler e InativarContaHandler

---
+++ backend/BankMore/ContaCorrente.Tests/CriarContaHandlerTests.cs
@@ -0,0 +1,99 @@
+﻿using BankMore.Infrastructure.Auth;
+using ContaCorrente.API.Application.Commands;
+using ContaCorrente.API.Application.Handlers;
+using ContaCorrente.API.Domain.Interfaces;
+using FluentAssertions;
+using Moq;
+
+namespace ContaCorrente.Tests
+{
+    public class CriarContaHandlerTests
+    {
+        private readonly Mock<IContaCorrenteRepository> _repository = new();
+        private readonly Mock<IPasswordService> _passwordService = new();
+
+        private readonly CriarContaHandler _handler;
+
+        public CriarContaHandlerTests()
+        {
+            _handler = new CriarContaHandler(
+                _repository.Object,
+                _passwordService.Object
+            );
+        }
+
+        [Fact]
+        public async Task Deve_Criar_Conta_Com_Sucesso()
+        {
+            // Arrange
+            var request = new CriarContaCommand
+            {
+                Cpf = "12345678909",
+                Senha = "123456"
+            };
+
+            _repository.Setup(x => x.ExistePorCpf(It.IsAny<string>()))
+                .ReturnsAsync(false);
+
+            _passwordService.Setup(x => x.Hash(It.IsAny<string>()))
+                .Returns("senha-hash");
+
+            // Act
+            var result = await _handler.Handle(request, default);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Data.Should().BeGreaterThan(0);
+
+            _repository.Verify(x => x.Criar(It.IsAny<API.Domain.Entities.ContaCorrente>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Deve_Falhar_Quando_Cpf_Invalido()
+        {
+            var request = new CriarContaCommand
+            {
+                Cpf = "123",
+                Senha = "123456"
+            };
+
+            var result = await _handler.Handle(request, default);
+
+            result.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Deve_Falhar_Quando_Cpf_Ja_Cadastrado()
+        {
+            var request = new CriarContaCommand
+            {
+                Cpf = "12345678909",
+                Senha = "123456"
+            };
+
+            _repository.Setup(x => x.ExistePorCpf(It.IsAny<string>()))
+                .ReturnsAsync(true);
+
+            var result = await _handler.Handle(request, default);
+
+            result.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Deve_Falhar_Quando_Senha_Invalida()
+        {
+            var request = new CriarContaCommand
+            {
+                Cpf = "12345678909",
+                Senha = ""
+            };
+
+            _repository.Setup(x => x.ExistePorCpf(It.IsAny<string>()))
+                .ReturnsAsync(false);
+
+            var result = await _handler.Handle(request, default);
+
+            result.IsSuccess.Should().BeFalse();
+        }
+    }
+}
---
+++ backend/BankMore/ContaCorrente.Tests/InativarContaHandlerTests.cs
@@ -0,0 +1,134 @@
+﻿using BankMore.Infrastructure.Auth;
+using ContaCorrente.API.Application.Commands;
+using ContaCorrente.API.Application.Handlers;
+using ContaCorrente.API.Domain.Interfaces;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+
+namespace ContaCorrente.Tests
+{
+    public class InativarContaHandlerTests
+    {
+        private readonly Mock<IContaCorrenteRepository> _repository = new();
+        private readonly Mock<IPasswordService> _passwordService = new();
+        private readonly Mock<IHttpContextAccessor> _httpContext = new();
+
+        private readonly InativarContaHandler _handler;
+
+        public InativarContaHandlerTests()
+        {
+            _handler = new InativarContaHandler(
+                _repository.Object,
+                _passwordService.Object,
+                _httpContext.Object
+            );
+        }
+
+        private void MockUsuario(string accountId)
+        {
+            var claims = new[]
+            {
+                new Claim("accountId", accountId)
+            };
+
+            var identity = new ClaimsIdentity(claims);
+            var user = new ClaimsPrincipal(identity);
+
+            var context = new DefaultHttpContext
+            {
+                User = user
+            };
+
+            _httpContext.Setup(x => x.HttpContext)
+                .Returns(context);
+        }
+
+        [Fact]
+        public async Task Deve_Inativar_Conta_Com_Sucesso()
+        {
+            // Arrange
+            var accountId = "123";
+
+            MockUsuario(accountId);
+
+            var conta = new API.Domain.Entities.ContaCorrente("cpf", "hash");
+
+            _repository.Setup(x => x.ObterPorId(accountId))
+                .ReturnsAsync(conta);
+
+            _passwordService.Setup(x => x.Verify("senha", "hash"))
+                .Returns(true);
+
+            var request = new InativarContaCommand
+            {
+                Senha = "senha"
+            };
+
+            // Act
+            var result = await _handler.Handle(request, default);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Deve_Falhar_Quando_Token_Invalido()
+        {
+            var request = new InativarContaCommand
+            {
+                Senha = "senha"
+            };
+
+            var result = await _handler.Handle(request, default);
+
+            result.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Deve_Falhar_Quando_Conta_Nao_Existe()
+        {
+            var accountId = "123";
+
+            MockUsuario(accountId);
+
+            _repository.Setup(x => x.ObterPorId(accountId))
+                .ReturnsAsync((API.Domain.Entities.ContaCorrente?)null);
+
+            var request = new InativarContaCommand
+            {
+                Senha = "senha"
+            };
+
+            var result = await _handler.Handle(request, default);
+
+            result.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Deve_Falhar_Quando_Senha_Invalida()
+        {
+            var accountId = "123";
+
+            MockUsuario(accountId);
+
+            var conta = new API.Domain.Entities.ContaCorrente("cpf", "hash");
+
+            _repository.Setup(x => x.ObterPorId(accountId))
+                .ReturnsAsync(conta);
+
+            _passwordService.Setup(x => x.Verify("senha", "hash"))
+                .Returns(false);
+
+            var request = new InativarContaCommand
+            {
+                Senha = "senha"
+            };
+
+            var result = await _handler.Handle(request, default);
+
+            result.IsSuccess.Should().BeFalse();
+        }
+    }
+}
